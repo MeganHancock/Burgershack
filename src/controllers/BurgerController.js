e9b8852ab1a1306cb584570af3b876e470c5950d
MeganHancock <mgnhnck@gmail.com>
2024-02-19 15:19:48 -0700

    getting burgers

--- src/controllers/BurgerController.js
+++ src/controllers/BurgerController.js
@@ -1,8 +1,28 @@
+import { burgerService } from "../services/BurgerService.js";
 import BaseController from "../utils/BaseController.js";
 
 export class BurgerController extends BaseController {
 
     constructor() {
-        super()
+        super('api/burgers');
+        this.router.get('', this.getBurgers)
+    }
+
+
+    /**
+ * Sends all values back to the client
+ * @param {import("express").Request} request
+ * @param {import("express").Response} response
+ * @param {import("express").NextFunction} next
+ */
+    async getBurgers(request, response, next) {
+        try {
+            // response.send('burger shack')
+            // @ts-ignore
+            const burgers = burgerService.getBurgers()
+            response.send(burgers)
+        } catch (error) {
+            next(error)
+        }
     }
 }
--- src/services/BurgerService.js
+++ src/services/BurgerService.js
@@ -1,5 +1,11 @@
+import { fakeDb } from "../db/FakeDb.js"
 
 class BurgerService {
+
+    getBurgers() {
+        const burgers = fakeDb.burgers
+        return burgers
+    }
 
 }
 
