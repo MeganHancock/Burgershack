138ea2580b56adc5d865906e0c4866a54846bcce
MeganHancock <mgnhnck@gmail.com>
2024-02-19 14:43:12 -0700

    burger class model

---
+++ src/models/Burger.js
@@ -0,0 +1,10 @@
+export class Burger {
+
+    constructor(data) {
+        this.id = data.id
+        this.name = data.name
+        this.price = data.price
+        this.hasCheese = data.hasCheese
+        this.hasBacon = data.hasBacon
+    }
+}
